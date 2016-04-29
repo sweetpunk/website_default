1d541e643920519f421f77d5738a70d5af6d380f
louis thoreau <louis@manomano.com>
2016-04-29 14:03:40 +0200

    remove useless

--- index.php
+++ index.php
@@ -6,17 +6,5 @@
  * Time: 11:30
  */
 
-require('lib/libs/Smarty/Smarty.class.php');
-$smarty = new Smarty();
-
-$smarty->setTemplateDir('/home/louis/PhpstormProjects/russian/smarty/templates');
-$smarty->setCompileDir('/home/louis/PhpstormProjects/russian/smarty/templates_c');
-$smarty->setCacheDir('/home/louis/PhpstormProjects/russian/smarty/cache');
-$smarty->setConfigDir('/home/louis/PhpstormProjects/russian/smarty/configs');
-
-$smarty->assign('test', 'Ned');
-$smarty->display('tpl/layout.tpl');
-
-
-//header('Location: php/layout.php');
-//exit;
+header('Location: php/layout.php');
+exit;
