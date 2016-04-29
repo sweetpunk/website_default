95848988c4bc47fdf48dc04672f1720d7f777b2e
louis thoreau <louis@manomano.com>
2016-04-29 11:48:32 +0200

    start

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: louis
+ * Date: 29/04/16
+ * Time: 11:30
+ */
+
+header('Location: php/layout.php');
+exit;
---
+++ php/layout.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: louis
+ * Date: 29/04/16
+ * Time: 11:34
+ */
+
+require_once '../tpl/layout.html';
